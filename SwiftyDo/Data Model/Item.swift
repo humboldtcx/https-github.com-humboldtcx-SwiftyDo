8f6415fe74c425203584a20a016596be7faf7111
James Williams <humboldtcx@gmail.com>
2018-08-10 14:02:27 -0700

    Convert to MVC design

---
+++ SwiftyDo/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  SwiftyDo
+//
+//  Created by James Williams on 8/10/18.
+//  Copyright © 2018 James Williams. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
